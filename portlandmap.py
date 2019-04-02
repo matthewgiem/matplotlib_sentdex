56c0003a26b5047bec34416093579ab964a27f59
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-04-02 18:02:06 +0700

    create first map

--- portlandmap.py
+++ portlandmap.py
@@ -4,17 +4,24 @@ import pandas as pd
 
 df = pd.read_csv('mapping_coordinates.csv')
 
-print()
+long = df['vehicle_location_longitude'].tolist()
+lat = df['vehicle_location_latitude'].tolist()
+lon_lat = []
+for y in range(len(lat)):
+    lon_lat.append([long[y], lat[y]])
 
-# m = Basemap(projection='mill',
-#             llcrnrlat = 45.417749,
-#             llcrnrlon = -122.822203,
-#             urcrnrlat = 45.643937,
-#             urcrnrlon = -122.484606,
-#             resolution = 'f')
-# # m.fillcontinents()
-# m.drawrivers()
-# m.drawcoastlines()
-#
-# plt.title('portland')
-# plt.show()
+# 45.526135, -122.675518
+
+m = Basemap(projection='mill',
+            llcrnrlat = 45.4,
+            llcrnrlon = -122.8,
+            urcrnrlat = 45.643937,
+            urcrnrlon = -122.484606,
+            resolution = 'f')
+# m.fillcontinents()
+m.plot(lon_lat[0], lon_lat[0], 'c*', markersize=15)
+m.drawrivers()
+m.drawcoastlines()
+
+plt.title('portland')
+plt.show()
