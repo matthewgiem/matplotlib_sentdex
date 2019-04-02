855cc86b39ae858fec38eea85931b8ca2b56a79a
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-04-02 14:15:27 +0700

    add comments

--- basemap.py
+++ basemap.py
@@ -1,10 +1,11 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-
+# https://matplotlib.org/basemap/ for documentation for Basemap
 m = Basemap(projection='mill')
-
+# mill stands for Miller Cylindrical Projection
+# more projections can be found at https://matplotlib.org/basemap/users/mapsetup.html
 m.drawcoastlines()
-m.fillcontinents() 
+m.fillcontinents()
 
 plt.title('Basemap Tutorial')
 plt.show()
